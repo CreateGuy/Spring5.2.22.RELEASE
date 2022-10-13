2f8dc8de97abe4372d9e1c89171a9364990eeb15
lzx <17623250093@163.com>
2022-10-13 14:28:31 +0800

    搭建网址

--- Demo/src/main/java/com/lzx/demo/Application.java
+++ Demo/src/main/java/com/lzx/demo/Application.java
@@ -15,6 +15,9 @@ import java.util.Arrays;
 @ComponentScan({"com.lzx.demo"})
 public class Application {
 	public static void main(String[] args) {
+		System.out.println("编译环境的代建：https://blog.csdn.net/Ber_Bai/article/details/121223572?ops_request_misc=&request_id=&biz_id=102&utm_term=springframework%20%E7%BC%96%E8%AF%91%E8%BF%90%E8%A1%8C&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduweb~default-5-121223572.nonecase&spm=1018.2226.3001.4187");
+		System.out.println("解决target目录没有提交的问题：https://blog.csdn.net/qq_35443962/article/details/121870667");
+
 		ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
 		MsgService msgService = (MsgServiceImpl) context.getBean("msg");
 		System.out.println(msgService.getMsg());
